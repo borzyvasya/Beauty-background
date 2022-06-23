2d9c75bbc574cb2f2ac098738bdda4575e485a9a
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2022-06-23 19:53:08 +0300

    Add files via upload

---
+++ pretty background/app.js
@@ -0,0 +1,11 @@
+VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  color1: 0x2e00ac,
+  color2: 0x36116b
+})
